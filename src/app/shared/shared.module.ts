e4e6dacb6c648e9664b77c2125bb4d5ca009727b
marylu <marylo281292@gmail.com>
2017-03-03 02:18:58 -0400

    header and menu

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HeaderComponent } from './header/header.component';
+import { MaterialModule } from '@angular/material';
 
 @NgModule({
   declarations: [
     HeaderComponent],
   imports: [
     BrowserModule,
+    MaterialModule
   ],
   exports: [
     HeaderComponent
