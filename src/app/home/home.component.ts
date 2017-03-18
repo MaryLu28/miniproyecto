c7d35b468bdf76ad30f00e82789efb9ef763f1b2
marylu <marylo281292@gmail.com>
2017-03-17 22:50:45 -0400

    Home ready

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HeaderService } from '../shared/header/header.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private headerSvc: HeaderService) { }
 
   ngOnInit() {
+    this.headerSvc.hideToolbar(true);
   }
+
+  tiles = [
+    { text: 'Demostrar', link:'/demostrar'},
+    { text: 'Mi perfil', link: '/'},
+    { text: 'Mis teoremas', link: '/'},
+    { text: 'Mis predicados y definiciones', link: '/'},
+    { text: 'Ayuda', link: '/' }
+  ];
 
 }
