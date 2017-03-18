fa13205867d17549dd1695a51751ba5a8d528e66
marylu <marylo281292@gmail.com>
2017-03-17 22:51:07 -0400

    created prove component

---
+++ src/app/prove/prove.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-prove',
+  templateUrl: './prove.component.html',
+  styleUrls: ['./prove.component.scss']
+})
+export class ProveComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
