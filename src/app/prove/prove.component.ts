b5ebc94c71ae72ff48d67f7057b8660d51807962
marylu <marylo281292@gmail.com>
2017-03-25 10:36:29 -0400

    Changes in prove component

--- src/app/prove/prove.component.ts
+++ src/app/prove/prove.component.ts
@@ -12,4 +12,23 @@ export class ProveComponent implements OnInit {
   ngOnInit() {
   }
 
+  methods = [
+    { value: 1, name: "Método directo" },
+    { value: 2, name: "Suposición del antcedente" },
+    { value: 3, name: "Cnotradicción" },
+  ];
+
+  theorems = [
+    { category: "Equevalencia", id: "1.1", theorem: "True === q === q" },
+    { category: "Equevalencia", id: "1.2", theorem: "p === ~p === False" },
+    { category: "Conjunción", id: "2.1", theorem: "p \\/ ~p" },
+    { category: "Disyunción", id: "3.1", theorem: "p /\\ False === False" }
+  ];
+
+  definitios = [
+    {  }
+  ]
+
+  
+
 }
